1ac259d5856a05c8a7c4005c89962c610d0320f4
Athul-k-ak <athulk8888@gmail.com>
2025-06-17 11:45:57 +0530

    deployment \

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hospital-project-backend-2.onrender.com/api",
   withCredentials: true, // ✅ Always send cookies
 });
 
